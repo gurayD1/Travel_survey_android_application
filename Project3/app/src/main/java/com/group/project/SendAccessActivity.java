a49078d5e560dcc802d62d721562d512e923450c
Vishkujo <vepicage@gmail.com>
2022-04-09 15:24:25 -0400

    Updated some stuff in the access and reset code layouts

---
+++ Project3/app/src/main/java/com/group/project/SendAccessActivity.java
@@ -0,0 +1,18 @@
+package com.group.project;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SendAccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_access_code);
+    }
+
+    public void submitAccessClicked(View view) {
+    }
+}
---
+++ Project3/app/src/main/java/com/group/project/SendResetActivity.java
@@ -0,0 +1,21 @@
+package com.group.project;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SendResetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_reset_code);
+    }
+
+    public void submitClicked(View view) {
+    }
+
+    public void accessCodeClicked(View view) {
+    }
+}
